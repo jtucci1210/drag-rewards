db014b89a40145185d082f80b465135c8498c407
jptucci <jptucci1210@gmail.com>
2020-06-03 11:30:32 -0400

    Create layout

--- src/App.js
+++ src/App.js
@@ -3,24 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const rewards = ["R1", "R2", "R3", "R4", "R5"];
+
+	const categories = ["C1", "C2", "C3", "C4", "C5"];
+
+	return (
+		<div className="App">
+			<div className="grid">
+				<div className="rewards">
+					<span className="reward-heading">Rewards</span>
+					<div className="reward-container">
+						{rewards.map((reward, idx) => {
+							return (
+								<div className="reward-item" key={idx}>{reward}</div>
+							)
+						})}
+					</div>
+				</div>
+				<div className="categories">
+					<span className="category-heading">Categories</span>
+					<div className="category-container">
+						{categories.map((category, idx) => {
+							return (
+								<div className="category-item" key={idx}>
+									<div className="category-name">{category}</div>
+									<div className="category-lane">
+
+									</div>
+								</div>
+							)
+						})}
+					</div>
+				</div>
+			</div>
+    	</div>
+  	);
 }
 
 export default App;
