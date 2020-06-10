396cecbe466925ab526238c2508788fea959a587
jptucci <jptucci1210@gmail.com>
2020-06-10 10:55:09 -0400

    Updates

---
+++ precache-manifest.31801538183498fe365306465c3edbf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c70f11a95a1e4bd4892d0395c5e3d31",
+    "url": "/drag-rewards/index.html"
+  },
+  {
+    "revision": "00170915a8daf886a6b2",
+    "url": "/drag-rewards/static/css/main.cfeaefa9.chunk.css"
+  },
+  {
+    "revision": "32cc766abd53831deb11",
+    "url": "/drag-rewards/static/js/2.4cd395e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drag-rewards/static/js/2.4cd395e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00170915a8daf886a6b2",
+    "url": "/drag-rewards/static/js/main.615e9d33.chunk.js"
+  },
+  {
+    "revision": "ea1600407bf2c5ac75b7",
+    "url": "/drag-rewards/static/js/runtime-main.ff2c9a94.js"
+  }
+]);
